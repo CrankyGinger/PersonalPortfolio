1cf44609e3294bdbc9b5b0a218118517babd5fd4
Robert Flath <robertflath396@gmail.com>
2022-04-21 20:26:59 -0700

    Added icons

--- portfolio.js
+++ portfolio.js
@@ -1,7 +1,4 @@
-var app = document.getElementsByTagName("BODY")[0];
-if (localStorage.lightMode == "dark") {
-    app.setAttribute("light-mode", "dark");
-}
+
 
 function toggle_light_mode() {
     var app = document.getElementsByTagName("BODY")[0];
@@ -11,13 +8,16 @@ function toggle_light_mode() {
     } else {
         localStorage.lightMode = "dark";
         app.setAttribute("light-mode", "dark");
-    }       
+    } 
+    
 }
+
 
 window.addEventListener("storage", function () {
     if (localStorage.lightMode == "dark") {
         app.setAttribute("light-mode", "dark");
-    } else {
-        app.setAttribute("light-mode", "light");
+    }else {
+        app.setAttribute("light-mode", "light")
     }
-}, false);
+});
+
